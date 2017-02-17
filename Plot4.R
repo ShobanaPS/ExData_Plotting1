592738f91b3830e3787398774eb2f99067707166
ShobanaPS <shobana.ps3@gmail.com>
2017-02-17 07:19:32 +0000

    Added comment about data file
    Added a comment that the txt data file used in the code needs to be in the working folder from which the code is run

--- Plot4.R
+++ Plot4.R
@@ -1,3 +1,5 @@
+##The txt file with data must be in the working directory from which the code will be executed
+
 # read data for rows 66638 through to 69,517 which have data for 1/2/2007 & 2/2/2007
 data <- read.delim("household_power_consumption.txt", header = FALSE, sep = ";", na.strings = "?", skip = 66637, nrows = 2880)
 headers <- read.delim("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?",nrows = 2)
